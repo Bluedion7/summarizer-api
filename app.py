d9c01cea3416e572b0b649ad9bfb10a0a70a1fc9
osa igbinedion <osa.igbinedion@ct.gov>
2025-05-03 15:43:54 -0400

    files-added2

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
 class TextInput(BaseModel):
     text: str
 
+
 @app.post("/summarize")
 def summarize_text(input: TextInput):
     summary = summarizer(input.text, max_length=130, min_length=30, do_sample=False)
